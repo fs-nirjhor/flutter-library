31fcc1bf8fa749619f792181392931d06a4263bc
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-04-05 22:06:25 +0600

    article

--- src/components/article/SingleArticle.jsx
+++ src/components/article/SingleArticle.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const SingleArticle = ({ article }) => {
   return (
-    <figure className="flex flex-col gap-2 rounded p-2 hover:shadow-lg">
-      <div className="relative w-full aspect-video mb-2">
+    <article className="flex flex-col gap-1 rounded p-2 hover:shadow-lg">
+      <figure className="relative w-full aspect-video mb-2">
         <Image
           src={article?.image}
           alt={article?.title}
@@ -11,13 +11,13 @@ const SingleArticle = ({ article }) => {
           placeholder="blur"
           blurDataURL={article?.image}
         />
-      </div>
-      <p className="flex items-center gap-5 text-gray-400">
+      </figure>
+      <p className="flex items-center gap-5 text-gray-400 text-sm">
         <span>{article?.date}</span>
         <span>{article?.category}</span>
       </p>
       <h4 className="font-bold">{article?.title}</h4>
-    </figure>
+    </article>
   );
 };
 export default SingleArticle;
